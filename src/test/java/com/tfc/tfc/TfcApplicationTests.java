49bf760bcbe7713c103811ed2526be0c9b228cb9
iagorodrigueznieto <156656151+iagorodrigueznieto@users.noreply.github.com>
2024-04-02 10:31:09 +0200

    Sumary

---
+++ src/test/java/com/tfc/tfc/TfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfc.tfc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
